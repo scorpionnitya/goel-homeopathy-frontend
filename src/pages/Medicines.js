eb89d62ad829da077cb6d980d4fb55e468e3015a
rajeev goel <rajeevgoel@rajeevs-MacBook-Air.local>
2026-04-25 14:01:46 +0530

    updated Biochemic label position

--- src/pages/Medicines.js
+++ src/pages/Medicines.js
@@ -16,7 +16,7 @@ const getLabelStyle = (tab) => {
     return { top: "65%", left: "50%", width: "44%", fontSize: "11px" };
 
   if (tab === "Biochemic")
-    return { top: "60%", left: "50%", width: "45%", fontSize: "20px" };
+    return { top: "60%", left: "50%", width: "25%", fontSize: "20px" };
 
   if (tab === "BC")
     return { top: "63%", left: "35%", width: "13%", fontSize: "15px" };
