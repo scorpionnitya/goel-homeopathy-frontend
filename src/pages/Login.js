d922ebeaa0e008f57abaecb4c09d57d220a0124a
rajeev goel <rajeevgoel@rajeevs-MacBook-Air.local>
2026-06-10 12:19:46 +0530

    Removed unused useNavigate import

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { FiMail, FiLock } from "react-icons/fi";
